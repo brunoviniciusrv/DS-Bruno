46e67249fb90f9aeef84f23135363d5461d28259
brunoviniciusrv <65190138+brunoviniciusrv@users.noreply.github.com>
2024-01-27 15:45:57 -0300

    teste
    2

--- backend/ticketing/src/main/java/dominios/api/ticketing/repo/TicketRepo.java
+++ backend/ticketing/src/main/java/dominios/api/ticketing/repo/TicketRepo.java
@@ -1,8 +1,13 @@
 package dominios.api.ticketing.repo;
 import dominios.api.ticketing.entity.Ticket;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TicketRepo extends MongoRepository<Ticket, String> {
+        List<Ticket> findByEventoId(String eventoId);
+        List<Ticket> findByUsuarioId(String usuarioId);
 }
